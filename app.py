b238e7d6435f09be01b6c72fd9272e5d667c943f
johith <johithx@gmail.com>
2023-04-28 18:06:19 +0530

    Made the base UI

--- app.py
+++ app.py
@@ -1,4 +1,5 @@
 from flask import Flask, render_template,request
+import json
 
 app = Flask(__name__)
 
@@ -6,7 +7,14 @@ app = Flask(__name__)
 def hello():
     return render_template('index.html')
 
-@app.route('/change', methods=['GET', 'POST'])
+@app.route('/change', methods=['POST'])
 def parse_request():
-    data=request.data
+    data=request.get_json()
     print(data)
+    return 
+@app.route('/about')
+def about():
+    return render_template('about.html')
+if __name__=='__main__':
+    app.run(debug=True)
+#curl -X POST -H "Content-Type: application/json" -d '{"key": "value"}' http://127.0.0.1:5000/change
---
+++ readme.md
@@ -0,0 +1,2 @@
+Testing
+curl -i -H "Content-Type: application/json" -X POST -d '{"slot1":"1", "slot2": "0"}' http://localhost:5000/change
